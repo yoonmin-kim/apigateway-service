ddcc6ac876c5a37b7f92d5d3354ebb90814eb035
yoonmin-kim <rladbsals23@gmail.com>
2021-08-26 23:16:50 +0900

    feat : Spring Cloud GateWay - CustomFilter 적용
    AbstractGatewayFilterFactory를 extends 받은 클래스를 작성한다.
    GateWayFilter를 반환타입으로 가지는 apply()메소드 오버라이드.
    
    application.yml에 spring.cloud.routes.filters 항목에 CustomFilter를 추가하거나
    FilterConfig에 작성했던 RouteLocator를 반환타입으로 가지는 빈에 다음과 같이 등록.
    @Bean
    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
        return builder.routes()
            .route(r -> r.path("/first-service/**")
                .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config())))
                .uri("http://localhost:8083"))
            .build();
    }

--- src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -1,19 +1,26 @@
 package com.example.apigatewayservice.config;
 
+import com.example.apigatewayservice.filter.CustomFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
+@RequiredArgsConstructor
 public class FilterConfig {
 
-//    @Bean
+    private final CustomFilter customFilter;
+
+    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
                             .filters(f -> f.addRequestHeader("first-request", "first-request-header")
                                             .addResponseHeader("first-response", "first-response-header"))
+//                            .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config())))
                             .uri("http://localhost:8083"))
                 .route(r -> r.path("/second-service/**")
                         .filters(f -> f.addRequestHeader("second-request", "second-request-header")
