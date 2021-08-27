431f7e78a695083ac97c029845f7eae745ac9456
yoonmin-kim <rladbsals23@gmail.com>
2021-08-27 22:41:49 +0900

    feat : Spring Cloud GateWay - LoggingFilter
    GatewayFilter의 구현체인 OrderedGatewayFilter를 정의하여 반환한다.
    OrderedGatewayFilter의 두번째 파라마터에 우선순위를 지정할 수 있다.
    
    [가장 높은 우선순위로 지정할 경우]
    Logging -> Global -> Custom
    [가장 낮은 우선순위로 지정할 경우]
    Global -> Custom -> Logging

--- src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -1,13 +1,9 @@
 package com.example.apigatewayservice.config;
 
 import com.example.apigatewayservice.filter.CustomFilter;
-import com.example.apigatewayservice.filter.GlobalFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 @RequiredArgsConstructor
